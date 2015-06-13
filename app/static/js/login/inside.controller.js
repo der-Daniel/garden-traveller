e882f6426bb35864e51e4afb4d1f3306abeef758
Philippe <physiognom@gmail.com>
2015-06-13 22:23:43 +0200

    fix design

--- app/static/js/login/inside.controller.js
+++ app/static/js/login/inside.controller.js
@@ -1,7 +1,9 @@
 var component = require('./login.module');
 
 class InsideController {
+  constructor($rootScope) {
 
+  }
 }
 
 component.controller('InsideController', InsideController);
--- app/static/js/login/login.route.js
+++ app/static/js/login/login.route.js
@@ -103,3 +103,9 @@ function Routes($stateProvider, $locationProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/home');
 
 }
+
+component.run(function($rootScope) {
+  $rootScope.$on('$stateChangeSuccess',function(event, toState, toParams, fromState, fromParams){
+    $rootScope.title = toState.title;
+  });
+});
