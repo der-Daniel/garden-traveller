dd7188f726b0fc9cd8a47d013744d710aa6414ff
Philippe <physiognom@gmail.com>
2015-06-13 14:34:36 +0200

    init

--- app/static/tasks/watch.js
+++ app/static/tasks/watch.js
@@ -4,7 +4,7 @@ var gulp          = require('gulp');
 
 gulp.task('watch', function() {
     gulp.watch('index.html', ['home']);
-    gulp.watch('js/**/*', ['browserify']);
-    gulp.watch('styles/**/*',  ['less']);
+    gulp.watch(['js/**/*.js', 'js/**/*.html'], ['browserify']);
+    gulp.watch('styles/**/*.less',  ['less']);
     gulp.watch('images/**/*',  ['images']);
 });
