cc9c9c157f1526770d3a2a80731a336f7709bbe0
Patrick Seemann <patsee14@gmail.com>
2015-06-13 13:01:44 +0200

    backend progress

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,26 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask.ext.bcrypt import Bcrypt
+from flask.ext.login import LoginManager
 
 
 app = Flask(__name__)
 app.config.from_object('app.settings')
 app.url_map.strict_slashes = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 # api
 api = Api(app)
 
 from app import models, views
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "signin"
+
+
+@login_manager.user_loader
+def load_user(userid):
+    return models.User.query.filter(models.User.id == userid).first()
--- app/views.py
+++ app/views.py
@@ -5,15 +5,63 @@ from flask import make_response, abort
 from flask import jsonify
 from flask import flash
 
-# from flask_restful import Resource
-# from flask_restful import reqparse
-# from app import api
+from flask_restful import Resource
+from flask_restful import reqparse
+from app import api
+
+
+loginParser = reqparse.RequestParser()
+loginParser.add_argument('email', type=str, required=True,
+                            help='Email address')
+loginParser.add_argument('password', type=str, required=True,
+                            help='password')
+
+
+
+# class Login(Resource):
+#
+#     def put(self):
+#         args = loginParser.parse_args()
+#         print('LOGIN ', args)
+#         return {'success': True}
+#
+# class Logout(Resource):
+#
+#     def put(self):
+#         return {'success': True}
+
+
+
+# api.add_resource(Login, '/api/login')
+# api.add_resource(Logout, '/api/logout')
+
+@app.route('/api/signup', methods = ['POST'])
+def signup():
+    return 'OK'
+
+@app.route('/api/signin', methods = ['POST'])
+def signin():
+    print('SIGNIN')
+
+    args = loginParser.parse_args()
+    print(dir(loginParser))
+    print('ARGS ', args)
+
+    # try:
+    #     email = request.form['email']
+    #     password = request.form['password']
+    #     print('EMAIL', email)
+    #     print('PASS', password)
+    # except KeyError as e:
+    #     print('KEY ERROR')
+    return '200'
+
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 #
 # Angular pages
