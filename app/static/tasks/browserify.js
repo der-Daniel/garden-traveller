5f66caa8bed55a25d921d44255038e7f9cae9116
Philippe <physiognom@gmail.com>
2015-06-13 14:35:14 +0200

    init

--- app/static/tasks/browserify.js
+++ app/static/tasks/browserify.js
@@ -2,18 +2,16 @@ var fs = require("fs");
 var gulp = require('gulp');
 var browserify = require("browserify");
 var babelify = require("babelify");
-var watchify = require('watchify');
 var gulpSequence = require('gulp-sequence');
 
 function bundleScript() {
-  var b = browserify({
+  browserify({
     entries: './js/main.js',
     debug: true,
     cache: {},
     packageCache: {},
     fullPaths: true
-  });
-  watchify(b)
+  })
     .transform(babelify)
     .bundle()
     .on("error", function (err) {
