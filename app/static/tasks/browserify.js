012fa9c2b1a1944ef80913f07f5fdd8e3bb28a15
Can Pekesen <can@pekesen.de>
2015-06-13 13:22:37 +0200

    watchify

--- app/static/tasks/browserify.js
+++ app/static/tasks/browserify.js
@@ -2,12 +2,17 @@ var fs = require("fs");
 var gulp = require('gulp');
 var browserify = require("browserify");
 var babelify = require("babelify");
+var watchify = require('watchify');
 
 function bundleScript() {
-  browserify({
+  var b = browserify({
     entries: './js/main.js',
-    debug: true
-  })
+    debug: true,
+    cache: {},
+    packageCache: {},
+    fullPaths: true
+  });
+  watchify(b)
     .transform(babelify)
     .bundle()
     .on("error", function (err) {
