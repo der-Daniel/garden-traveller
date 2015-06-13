623452ed51608bbcf55e3fb5c8cd9b0ce283b56e
derDaniel <der-daniel@outlook.com>
2015-06-13 15:12:22 +0200

    added homepage

---
+++ db_tools.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+#=========== imports =========== 
+
+import sqlite3 as sql
+
+# =============================
+
+
+con = lite.connect('database')
+cur = con.cursor() 
+
+
+
+
+
+# user I/O
+
+def add_user():
+		
+def edit_user():
+	
+def del_user():
+	
+
+# garden I/O	
+
+def add_garden():
+	
+def edit_garden():
+	
+def del_garden():
+	
+	
+# offering I/O
+	
+def add_offer():
+		
+def edit_offer():
+	
+def del_offer():
+	
+	
