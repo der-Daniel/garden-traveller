152f6c35a303bdcf4fccd3e99fd3d634c8c0d131
Can Pekesen <can@pekesen.de>
2015-06-13 12:09:32 +0200

    gulp

--- app/static/tasks/browserify.js
+++ app/static/tasks/browserify.js
@@ -1,3 +1,24 @@
-/**
- * Created by can on 13.06.15.
- */
+var fs = require("fs");
+var gulp = require('gulp');
+var browserify = require("browserify");
+var babelify = require("babelify");
+
+function bundleScript() {
+  browserify({
+    entries: '../js/main.js',
+    debug: true
+  })
+    .transform(babelify.configure({
+      extensions: [".es6"]
+    }))
+    .bundle()
+    .on("error", function (err) {
+      console.log("Error : " + err.message);
+    })
+    .pipe(fs.createWriteStream("../dist/main.js"));
+}
+
+
+gulp.task('browserify', function() {
+  return bundleScript();
+});
