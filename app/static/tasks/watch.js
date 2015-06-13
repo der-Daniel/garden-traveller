6c48240664816caf6e70d772f01546d208cc1266
Philippe <physiognom@gmail.com>
2015-06-13 20:08:53 +0200

    fix design

--- app/static/tasks/watch.js
+++ app/static/tasks/watch.js
@@ -4,7 +4,7 @@ var gulp          = require('gulp');
 
 gulp.task('watch', function() {
     gulp.watch('index.html', ['home']);
-    gulp.watch(['js/**/*.js', 'js/**/*.html'], ['browserify']);
+    gulp.watch(['js/**/*.js', 'js/**/*.html', '!js/templatecache.js'], ['browserify']);
     gulp.watch('styles/**/*.less',  ['styles']);
     gulp.watch('images/**/*',  ['images']);
 });
