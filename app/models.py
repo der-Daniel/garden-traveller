b555ce4c1e6e4fc3784516f031237fe0e55d2fe3
derDaniel <der-daniel@outlook.com>
2015-06-13 12:59:19 +0200

    added schema to models.py

--- app/models.py
+++ app/models.py
@@ -6,8 +6,10 @@ from flask.ext.login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String(128), unique=True)
-    _password = db.Column(db.String(128))
+    surname = db.Column(db.String(128), nullable=False)
+    name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    _password = db.Column(db.String(128), nullable=False)
 
     @hybrid_property
     def password(self):
@@ -19,3 +21,37 @@ class User(db.Model, UserMixin):
 
     def is_correct_password(self, plaintext):
         return bcrypt.check_password_hash(self._password, plaintext)
+
+    address = db.Column(db.Integer, db.ForeignKey(Address.id), nullable=False)
+    offerings = db.realtionship('Offering', backref=db.backref('user', lazy='joined'), lazy='dynamic')
+    gardens = db.relationship('Garden', backref=db.backref('garden', lazy='joined'), lazy='dynamic')
+    
+    
+    
+class Offering(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    product = db.Column(db.String(128), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.String(128), nullable=False)
+    date = DateTime(default=datetime.datetime.utcnow)
+    garden = db.Column(db.Integer, db.ForeignKey(Garden.id), nullable=False)
+    
+    
+class Garden(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    address = db.Column(db.Integer, db.ForeignKey(Address.id), nullable=False)
+    location = db.Column(db.Integer, db.ForeignKey(Location.id), nullable=False)
+    
+
+class Address(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    street = db.Column(db.String(128), nullable=False)
+    zipcode = db.Column(db.String(128), nullable=False)
+    house_number = db.Column(db.String(128), nullable=False)
+    city = db.Column(db.String(128), nullable=False)
+    
+    
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    longitude = db.Column(db.String(128), nullable=False)
+    latitude = db.Column(db.String(128), nullable=False)
